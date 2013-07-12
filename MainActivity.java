47085ce2efb8fff65dc62203459e7c64012250d5
SBF22 <box.sergei@gmail.com>
2013-07-12 16:51:51 +1100

    3

--- MainActivity.java
+++ MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+// Spartak chempion
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
